498ba28c641b0cca4ce3f9fcb7d50a0bf617479a
David Soergel <dev@davidsoergel.com>
2010-10-25 15:51:44 +0000

    aeouoeu

---
+++ ncbitaxonomyClient/src/test/java/edu/berkeley/compbio/ncbitaxonomy/service/NcbiTaxonomyClientTest.java
@@ -0,0 +1,28 @@
+package edu.berkeley.compbio.ncbitaxonomy.service;
+
+import com.davidsoergel.trees.NoSuchNodeException;
+import org.apache.log4j.Logger;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public class NcbiTaxonomyClientTest
+	{
+	private static final Logger logger = Logger.getLogger(NcbiTaxonomyClientTest.class);
+
+	@BeforeTest
+	public void setUp()
+		{
+		}
+
+	@Test
+	public void testRothia() throws NoSuchNodeException
+		{
+		String name = "Rothia mucilaginosa DY-18";
+		int id = NcbiTaxonomyClient.getInstance().findTaxidByName(name);
+		logger.info(name + " -> " + id);
+		}
+	}
